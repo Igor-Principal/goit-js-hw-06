7f745d264a81695222fed9ba726a61dc34844a15
Igor-Principal <igorprincipal@gmail.com>
2023-10-19 00:03:27 +0300

    Create task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onclick);
 
+function onclick(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+  if (email.value === "" || password.value === "") {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    form.reset();
+    console.log(data);
+  }
+}
