b013ec517b17a847ae2bb5d37b1346501173c8cc
Igor-Principal <igorprincipal@gmail.com>
2023-10-19 18:58:15 +0300

    Create task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChange = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+const body = document.querySelector("body");
+buttonChange.addEventListener("click", changeColor);
+
+function changeColor(evt) {
+  // за умови що функція не буде більше використовуватись її можна лишити тут
+  // function getRandomHexColor() {
+  //   return `#${Math.floor(Math.random() * 16777215)
+  //     .toString(16)
+  //     .padStart(6, 0)}`;
+  // }
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
+}
