0fd3cb35fd8a65c921a09f8e08ae7b51c3505128
Igor-Principal <igorprincipal@gmail.com>
2023-10-20 19:08:20 +0300

    create task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const input = document.querySelector('input[type="number"]');
+const container = document.querySelector("#controls");
+createButton.addEventListener("click", create);
+destroyButton.addEventListener("click", destroy);
+let amount = input.value;
+
+
+
+function create(evt) {
+  let amount = input.value;
+  destroy();
+  createBoxes(amount);
+}
+function createBoxes(amount) {
+  for (let i = Number(amount); i > 0; i -= 1) {
+    const example = `<div class="box" style="width:${30 + 10 * i}px; height:${
+      30 + 10 * i
+    }px; background-color:${getRandomHexColor()}"></div>`;
+    container.insertAdjacentHTML("beforeend", example);
+  }
+}
+
+function destroy() {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.remove();
+  });
+}
