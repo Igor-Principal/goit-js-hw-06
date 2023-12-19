dcde5e7c917b49477093dcf0daaf37b21b816ec7
Igor-Principal <igorprincipal@gmail.com>
2023-12-19 21:37:17 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,12 @@
 const items = document.querySelectorAll('.item')
+
 console.log(`Number of categories: ${items.length}`);
+
 items.forEach(item => {
+    
     const title = item.querySelector('h2').textContent;
     const elements = item.querySelectorAll('li').length
+
     console.log(`Category: ${title} `);
     console.log(`Elements: ${elements}`);
 })
